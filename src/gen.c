7b5895599b4bbc0eec6b33d61e63261a304a666c
vie <vie.devr@gmail.com>
2024-03-17 14:20:31 +0300

    Split code generation into separate functions

--- src/gen.c
+++ src/gen.c
@@ -72,236 +72,250 @@ typedef struct {
   i32           ifs_count;
 } Generator;
 
+static Str gen_expr_linux_x86_64(Generator *gen, Expr expr, Str target, bool strict);
+
+static Str gen_bin_op_linux_x86_64(Generator *gen, ExprBinOp *bin_op, Str target) {
+  if (str_eq(bin_op->op, STR("+", 1))
+      || str_eq(bin_op->op, STR("-", 1))
+      || str_eq(bin_op->op, STR("*", 1))) {
+    /* Yes, I didn't come up with a better name */
+    bool flag = str_eq(target, STR("rax", 3)) &&
+                bin_op->rhs.kind == ExprKindCall;
+
+    Str lhs_loc = target;
+    if (flag)
+      lhs_loc = mem_reserve(&gen->mem);
+    Str lhs = gen_expr_linux_x86_64(gen, bin_op->lhs, lhs_loc, true);
+
+    Str rhs_loc = mem_reserve(&gen->mem);
+    Str rhs = gen_expr_linux_x86_64(gen, bin_op->rhs,
+                                    rhs_loc, false);
+
+    mem_free(&gen->mem, lhs_loc);
+    mem_free(&gen->mem, rhs_loc);
+
+    if (str_eq(bin_op->op, STR("+", 1)))
+      sb_push(&gen->sb, "    add ");
+    else if (str_eq(bin_op->op, STR("-", 1)))
+      sb_push(&gen->sb, "    sub ");
+    else
+      sb_push(&gen->sb, "    imul ");
+    sb_push_str(&gen->sb, lhs);
+    sb_push(&gen->sb, ", ");
+    sb_push_str(&gen->sb, rhs);
+    sb_push(&gen->sb, "\n");
+
+    if (flag) {
+      sb_push(&gen->sb, "    mov rax, ");
+      sb_push_str(&gen->sb, lhs_loc);
+      sb_push(&gen->sb, "\n");
+    }
+
+    return lhs;
+  }
+
+  ERROR("Unknown operator: `");
+  str_fprint(stderr, bin_op->op);
+  fprintf(stderr, "`\n");
+  exit(1);
+}
+
+static Str gen_lit_linux_x86_64(Generator *gen, ExprLit *lit, Str target, bool strict) {
+  if (lit->kind == LitKindInt) {
+    if (!strict)
+      return lit->lit;
+
+    if (str_eq(lit->lit, STR("0", 1))) {
+      sb_push(&gen->sb, "    xor ");
+      sb_push_str(&gen->sb, target);
+      sb_push(&gen->sb, ", ");
+      sb_push_str(&gen->sb, target);
+      sb_push(&gen->sb, "\n");
+    } else {
+      sb_push(&gen->sb, "    mov ");
+      sb_push_str(&gen->sb, target);
+      sb_push(&gen->sb, ", ");
+      sb_push_str(&gen->sb, lit->lit);
+      sb_push(&gen->sb, "\n");
+    }
+
+    return target;
+  } else if (lit->kind == LitKindStr) {
+    StringBuilder sb;
+    sb_push(&sb, "db_");
+    sb_push_i32(&sb, gen->dbs.len);
+    DB db = {
+      .name = (Str) {
+        .ptr = sb.buffer,
+        .len = sb.len,
+      },
+      .data = lit->lit,
+    };
+    DA_APPEND(gen->dbs, db);
+
+    if (!strict)
+      return db.name;
+
+    sb_push(&gen->sb, "    mov ");
+    sb_push_str(&gen->sb, target);
+    sb_push(&gen->sb, ", ");
+    sb_push_str(&gen->sb, db.name);
+    sb_push(&gen->sb, "\n");
+
+    return target;
+  }
+
+  ERROR("Unreachable\n");
+  exit(1);
+}
+
+static Str gen_block_linux_x86_64(Generator *gen, ExprBlock *block, Str target, bool strict) {
+  Str mem = mem_reserve(&gen->mem);
+
+  for (i32 i = 0; i + 1 < block->len; ++i)
+    gen_expr_linux_x86_64(gen, block->items[i],
+                          mem, false);
+                          mem_free(&gen->mem, mem);
+
+  if (block->len > 0) {
+    Expr last = block->items[block->len - 1];
+    return gen_expr_linux_x86_64(gen, last, target, strict);
+  }
+
+  return STR("rax", 3);
+}
+
+static Str gen_ident_linux_x86_64(Generator *gen, ExprIdent *ident, Str target, bool strict) {
+  Str loc = STR("", 0);
+  Expr target_expr = ident->target_expr;
+  if (target_expr.kind == ExprKindVar)
+    loc = target_expr.as.var->loc;
+  else if (target_expr.kind == ExprKindFunc)
+    loc = target_expr.as.func->name;
+
+  if (!strict)
+    return loc;
+
+  sb_push(&gen->sb, "    mov ");
+  sb_push_str(&gen->sb, target);
+  sb_push(&gen->sb, ", ");
+  sb_push_str(&gen->sb, loc);
+  sb_push(&gen->sb, "\n");
+
+  return target;
+}
+
+static Str gen_call_linux_x86_64(Generator *gen, ExprCall *call, Str target, bool strict) {
+  for (i32 i = 0; i < call->args->len; ++i) {
+    Expr arg_expr = call->args->items[i];
+    Str arg_loc = mem_reserve_arg(&gen->mem);
+    gen_expr_linux_x86_64(gen, arg_expr, arg_loc, true);
+  }
+
+  Str callee = gen_expr_linux_x86_64(gen, call->func,
+                                     STR("rax", 3), false);
+
+  sb_push(&gen->sb, "    call ");
+  sb_push_str(&gen->sb, callee);
+  sb_push(&gen->sb, "\n");
+
+  if (strict && !str_eq(target, STR("rax", 3))) {
+    sb_push(&gen->sb, "    mov ");
+    sb_push_str(&gen->sb, target);
+    sb_push(&gen->sb, ", rax\n");
+  }
+
+  mem_free_args(&gen->mem);
+  return strict ? target : STR("rax", 3);
+}
+
+static Str gen_var_linux_x86_64(Generator *gen, ExprVar *var, Str target, bool strict) {
+  gen->stack_pointer += var->size;
+
+  StringBuilder sb = {0};
+  sb_push(&sb, "qword [rsp + ");
+  sb_push_i32(&sb, gen->scope_size - gen->stack_pointer);
+  sb_push(&sb, "]");
+  var->loc = (Str) {
+    .ptr = sb.buffer,
+    .len = sb.len,
+  };
+
+  Str value = gen_expr_linux_x86_64(gen, var->value, target, strict);
+
+  if (!str_eq(value, var->loc)) {
+    sb_push(&gen->sb, "    mov ");
+    sb_push_str(&gen->sb, var->loc);
+    sb_push(&gen->sb, ", ");
+    sb_push_str(&gen->sb, value);
+    sb_push(&gen->sb, "\n");
+  }
+
+  return value;
+}
+
+static Str gen_func_linux_x86_64(Generator *gen, ExprFunc *func, Str target, bool strict) {
+  func->loc = func->name;
+
+  if (!strict)
+    return func->name;
+
+  sb_push(&gen->sb, "    mov ");
+  sb_push_str(&gen->sb, target);
+  sb_push(&gen->sb, ", ");
+  sb_push_str(&gen->sb, func->name);
+  sb_push(&gen->sb, "\n");
+
+  return target;
+}
+
+static Str gen_if_linux_x86_64(Generator *gen, ExprIf *eef, Str target) {
+  Str cond = gen_expr_linux_x86_64(gen, eef->cond,
+                                   target, true);
+  i32 if_id = gen->ifs_count++;
+
+  sb_push(&gen->sb, "    cmp ");
+  sb_push_str(&gen->sb, cond);
+  sb_push(&gen->sb, ", 0\n");
+  sb_push(&gen->sb, "    je else_");
+  sb_push_i32(&gen->sb, if_id);
+  sb_push(&gen->sb, "\n");
+
+  gen_expr_linux_x86_64(gen, eef->body,
+                        target, true);
+
+  if (eef->has_else) {
+    sb_push(&gen->sb, "    jmp next_");
+    sb_push_i32(&gen->sb, if_id);
+    sb_push(&gen->sb, "\n");
+  }
+  sb_push(&gen->sb, "  else_");
+  sb_push_i32(&gen->sb, if_id);
+  sb_push(&gen->sb, ":\n");
+
+  if (eef->has_else)
+    gen_expr_linux_x86_64(gen, eef->elze, target, true);
+
+  sb_push(&gen->sb, "  next_");
+  sb_push_i32(&gen->sb, if_id);
+  sb_push(&gen->sb, ":\n");
+
+  return target;
+}
+
 static Str gen_expr_linux_x86_64(Generator *gen, Expr expr, Str target, bool strict) {
   switch (expr.kind) {
-  case ExprKindBlock: {
-    Str mem = mem_reserve(&gen->mem);
-    for (i32 i = 0; i + 1 < expr.as.block->len; ++i)
-      gen_expr_linux_x86_64(gen, expr.as.block->items[i],
-                            mem, false);
-    mem_free(&gen->mem, mem);
-
-    if (expr.as.block->len > 0) {
-      Expr last = expr.as.block->items[expr.as.block->len - 1];
-      return gen_expr_linux_x86_64(gen, last, target, strict);
-    }
-    return STR("rax", 3);
+  case ExprKindBinOp: return gen_bin_op_linux_x86_64(gen, expr.as.bin_op, target);
+  case ExprKindLit:   return gen_lit_linux_x86_64(gen, expr.as.lit, target, strict);
+  case ExprKindBlock: return gen_block_linux_x86_64(gen, expr.as.block, target, strict);
+  case ExprKindIdent: return gen_ident_linux_x86_64(gen, expr.as.ident, target, strict);
+  case ExprKindCall:  return gen_call_linux_x86_64(gen, expr.as.call, target, strict);
+  case ExprKindVar:   return gen_var_linux_x86_64(gen, expr.as.var, target, strict);
+  case ExprKindFunc:  return gen_func_linux_x86_64(gen, expr.as.func, target, strict);
+  case ExprKindIf:    return gen_if_linux_x86_64(gen, expr.as.eef, target);
   }
 
-  case ExprKindLit: {
-    if (expr.as.lit->kind == LitKindInt) {
-      if (!strict)
-        return expr.as.lit->lit;
-
-      if (str_eq(expr.as.lit->lit, STR("0", 1))) {
-        sb_push(&gen->sb, "    xor ");
-        sb_push_str(&gen->sb, target);
-        sb_push(&gen->sb, ", ");
-        sb_push_str(&gen->sb, target);
-        sb_push(&gen->sb, "\n");
-      } else {
-        sb_push(&gen->sb, "    mov ");
-        sb_push_str(&gen->sb, target);
-        sb_push(&gen->sb, ", ");
-        sb_push_str(&gen->sb, expr.as.lit->lit);
-        sb_push(&gen->sb, "\n");
-      }
-
-      return target;
-    } else if (expr.as.lit->kind == LitKindStr) {
-      StringBuilder sb;
-      sb_push(&sb, "db_");
-      sb_push_i32(&sb, gen->dbs.len);
-      DB db = {
-        .name = (Str) {
-          .ptr = sb.buffer,
-          .len = sb.len,
-        },
-        .data = expr.as.lit->lit,
-      };
-      DA_APPEND(gen->dbs, db);
-
-      if (!strict)
-        return db.name;
-
-      sb_push(&gen->sb, "    mov ");
-      sb_push_str(&gen->sb, target);
-      sb_push(&gen->sb, ", ");
-      sb_push_str(&gen->sb, db.name);
-      sb_push(&gen->sb, "\n");
-
-      return target;
-    }
-
-    ERROR("Unreachable\n");
-    exit(1);
-  }
-
-  case ExprKindBinOp: {
-    if (str_eq(expr.as.bin_op->op, STR("+", 1))
-        || str_eq(expr.as.bin_op->op, STR("-", 1))
-        || str_eq(expr.as.bin_op->op, STR("*", 1))) {
-      /* Yes, I didn't come up with a better name */
-      bool flag = str_eq(target, STR("rax", 3)) &&
-                  expr.as.bin_op->rhs.kind == ExprKindCall;
-
-      Str lhs_loc = target;
-      if (flag)
-        lhs_loc = mem_reserve(&gen->mem);
-      Str lhs = gen_expr_linux_x86_64(gen, expr.as.bin_op->lhs, lhs_loc, true);
-
-      Str rhs_loc = mem_reserve(&gen->mem);
-      Str rhs = gen_expr_linux_x86_64(gen, expr.as.bin_op->rhs,
-                                      rhs_loc, false);
-
-      mem_free(&gen->mem, lhs_loc);
-      mem_free(&gen->mem, rhs_loc);
-
-      if (str_eq(expr.as.bin_op->op, STR("+", 1)))
-        sb_push(&gen->sb, "    add ");
-      else if (str_eq(expr.as.bin_op->op, STR("-", 1)))
-        sb_push(&gen->sb, "    sub ");
-      else
-        sb_push(&gen->sb, "    imul ");
-      sb_push_str(&gen->sb, lhs);
-      sb_push(&gen->sb, ", ");
-      sb_push_str(&gen->sb, rhs);
-      sb_push(&gen->sb, "\n");
-
-      if (flag) {
-        sb_push(&gen->sb, "    mov rax, ");
-        sb_push_str(&gen->sb, lhs_loc);
-        sb_push(&gen->sb, "\n");
-      }
-
-      return lhs;
-    }
-
-    ERROR("Unknown operator: `");
-    str_fprint(stderr, expr.as.bin_op->op);
-    fprintf(stderr, "`\n");
-    exit(1);
-  }
-
-  case ExprKindIdent: {
-    Str loc = STR("", 0);
-    Expr target_expr = expr.as.ident->target_expr;
-    if (target_expr.kind == ExprKindVar)
-      loc = target_expr.as.var->loc;
-    else if (target_expr.kind == ExprKindFunc)
-      loc = target_expr.as.func->name;
-
-    if (!strict)
-      return loc;
-
-    sb_push(&gen->sb, "    mov ");
-    sb_push_str(&gen->sb, target);
-    sb_push(&gen->sb, ", ");
-    sb_push_str(&gen->sb, loc);
-    sb_push(&gen->sb, "\n");
-
-    return target;
-  }
-
-  case ExprKindVar: {
-    gen->stack_pointer += expr.as.var->size;
-
-    StringBuilder sb = {0};
-    sb_push(&sb, "qword [rsp + ");
-    sb_push_i32(&sb, gen->scope_size - gen->stack_pointer);
-    sb_push(&sb, "]");
-    expr.as.var->loc = (Str) {
-      .ptr = sb.buffer,
-      .len = sb.len,
-    };
-
-    Str value = gen_expr_linux_x86_64(gen, expr.as.var->value, target, strict);
-
-    if (!str_eq(value, expr.as.var->loc)) {
-      sb_push(&gen->sb, "    mov ");
-      sb_push_str(&gen->sb, expr.as.var->loc);
-      sb_push(&gen->sb, ", ");
-      sb_push_str(&gen->sb, value);
-      sb_push(&gen->sb, "\n");
-    }
-
-    return value;
-  }
-
-  case ExprKindCall: {
-    for (i32 i = 0; i < expr.as.call->args->len; ++i) {
-      Expr arg_expr = expr.as.call->args->items[i];
-      Str arg_loc = mem_reserve_arg(&gen->mem);
-      gen_expr_linux_x86_64(gen, arg_expr, arg_loc, true);
-    }
-
-    Str callee = gen_expr_linux_x86_64(gen, expr.as.call->func,
-                                       STR("rax", 3), false);
-    sb_push(&gen->sb, "    call ");
-    sb_push_str(&gen->sb, callee);
-    sb_push(&gen->sb, "\n");
-
-    if (strict && !str_eq(target, STR("rax", 3))) {
-      sb_push(&gen->sb, "    mov ");
-      sb_push_str(&gen->sb, target);
-      sb_push(&gen->sb, ", rax\n");
-    }
-
-    mem_free_args(&gen->mem);
-    return strict ? target : STR("rax", 3);
-  }
-
-  case ExprKindFunc: {
-    expr.as.func->loc = expr.as.func->name;
-
-    if (!strict)
-      return expr.as.func->name;
-
-    sb_push(&gen->sb, "    mov ");
-    sb_push_str(&gen->sb, target);
-    sb_push(&gen->sb, ", ");
-    sb_push_str(&gen->sb, expr.as.func->name);
-    sb_push(&gen->sb, "\n");
-
-    return target;
-  }
-
-  case ExprKindIf: {
-    Str cond = gen_expr_linux_x86_64(gen, expr.as.eef->cond,
-                                     target, true);
-    i32 if_id = gen->ifs_count++;
-
-    sb_push(&gen->sb, "    cmp ");
-    sb_push_str(&gen->sb, cond);
-    sb_push(&gen->sb, ", 0\n");
-    sb_push(&gen->sb, "    je else_");
-    sb_push_i32(&gen->sb, if_id);
-    sb_push(&gen->sb, "\n");
-
-    gen_expr_linux_x86_64(gen, expr.as.eef->body,
-                          target, true);
-
-    if (expr.as.eef->has_else) {
-      sb_push(&gen->sb, "    jmp next_");
-      sb_push_i32(&gen->sb, if_id);
-      sb_push(&gen->sb, "\n");
-    }
-    sb_push(&gen->sb, "  else_");
-    sb_push_i32(&gen->sb, if_id);
-    sb_push(&gen->sb, ":\n");
-
-    if (expr.as.eef->has_else)
-      gen_expr_linux_x86_64(gen, expr.as.eef->elze, target, true);
-
-    sb_push(&gen->sb, "  next_");
-    sb_push_i32(&gen->sb, if_id);
-    sb_push(&gen->sb, ":\n");
-
-    return target;
-  }
-  }
-
-  ERROR("Not implemented\n");
+  ERROR("Unreachable\n");
   exit(1);
 }
 
