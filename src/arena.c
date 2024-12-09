af08117b76b0e18af3e2c82508027395305d463d
vie <vie.devr@gmail.com>
2024-12-09 20:40:48 +0300

    Fixed but in arena allocator

--- src/arena.c
+++ src/arena.c
@@ -2,40 +2,44 @@
 
 #include "arena.h"
 
-#define ARENA_CAP 256
+#define DEFAULT_ARENA_CAP 256
 
 typedef struct Arena Arena;
 
 struct Arena {
-  u8     buffer[ARENA_CAP];
-  u32    used;
+  u8    *buffer;
+  u32    cap, len;
   Arena *next;
 };
 
 static Arena *arena = NULL;
+static Arena *last_arena = NULL;
 
-static Arena *create_arena() {
-  Arena *_arena = malloc(sizeof(Arena));
-  _arena->used = 0;
+static Arena *create_arena(u32 cap) {
+  Arena *_arena = malloc(sizeof(Arena) + cap);
+  _arena->buffer = (u8 *) _arena + sizeof(Arena);
+  _arena->cap = cap;
+  _arena->len = 0;
   _arena->next = NULL;
+  last_arena = _arena;
   return _arena;
 }
 
 void *aalloc(u32 amount) {
-  if (amount > ARENA_CAP)
-    return NULL;
+  if (amount > DEFAULT_ARENA_CAP)
+    arena = create_arena(amount);
   if (!arena)
-    arena = create_arena();
+    arena = create_arena(DEFAULT_ARENA_CAP);
 
   Arena *_arena = arena;
 
-  while (_arena->used + amount > ARENA_CAP) {
+  while (_arena->len + amount > _arena->cap) {
     if (!_arena->next)
-      _arena->next = create_arena();
+      _arena->next = create_arena(DEFAULT_ARENA_CAP);
     _arena = _arena->next;
   }
 
-  u32 used = _arena->used;
-  _arena->used += amount;
-  return _arena->buffer + used;
+  u32 len = _arena->len;
+  _arena->len += amount;
+  return _arena->buffer + len;
 }
